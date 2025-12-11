97100b445826286b2263ac9d47a19d87c957dd1b
Theo <theo.chevalier@campuscarloacutis.fr>
2025-12-11 11:45:26 +0100

    <feature> bdd avec jeu de test

--- dbecole.sql
+++ dbecole.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Dec 04, 2025 at 11:21 AM
--- Server version: 8.3.0
--- PHP Version: 8.2.18
+-- Hôte : 127.0.0.1:3306
+-- Généré le : jeu. 11 déc. 2025 à 10:42
+-- Version du serveur : 8.3.0
+-- Version de PHP : 8.2.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `dbecole`
+-- Base de données : `dbecole`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_admin`
+-- Structure de la table `tb_admin`
 --
 
 DROP TABLE IF EXISTS `tb_admin`;
@@ -36,29 +36,42 @@ CREATE TABLE IF NOT EXISTS `tb_admin` (
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `tb_admin`
+-- Déchargement des données de la table `tb_admin`
 --
 
 INSERT INTO `tb_admin` (`id`, `login`, `mdp`) VALUES
-(1, 'Admin1', 'Test');
+(1, 'admin1', 'adminpass123');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_classe`
+-- Structure de la table `tb_classe`
 --
 
 DROP TABLE IF EXISTS `tb_classe`;
 CREATE TABLE IF NOT EXISTS `tb_classe` (
   `idClasse` int NOT NULL AUTO_INCREMENT,
   `LibelleClasse` varchar(50) NOT NULL,
+  `Niveaux` varchar(10) NOT NULL,
   PRIMARY KEY (`idClasse`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `tb_classe`
+--
+
+INSERT INTO `tb_classe` (`idClasse`, `LibelleClasse`, `Niveaux`) VALUES
+(3, '1er1', '1er'),
+(4, '1er2', '1er'),
+(5, '2nd1', '2nd'),
+(6, '2nd2', '2nd'),
+(7, 'term1', 'terminale'),
+(8, 'term2', 'terminale');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_controle`
+-- Structure de la table `tb_controle`
 --
 
 DROP TABLE IF EXISTS `tb_controle`;
@@ -69,12 +82,20 @@ CREATE TABLE IF NOT EXISTS `tb_controle` (
   `description` varchar(255) DEFAULT NULL,
   `date` date NOT NULL,
   PRIMARY KEY (`idControle`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `tb_controle`
+--
+
+INSERT INTO `tb_controle` (`idControle`, `Coef`, `titre`, `description`, `date`) VALUES
+(1, 1, 'chapitre 1', 'Controle sur le cour de math', '2025-01-15'),
+(2, 3, 'chapitre 2', 'controle sur le cour d\'histoire', '2025-02-03');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_eleve`
+-- Structure de la table `tb_eleve`
 --
 
 DROP TABLE IF EXISTS `tb_eleve`;
@@ -87,19 +108,20 @@ CREATE TABLE IF NOT EXISTS `tb_eleve` (
   `idClasseEleve` int NOT NULL,
   PRIMARY KEY (`idEleve`),
   KEY `FK1` (`idClasseEleve`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `tb_eleve`
+-- Déchargement des données de la table `tb_eleve`
 --
 
 INSERT INTO `tb_eleve` (`idEleve`, `nomEleve`, `prenomEleve`, `loginEleve`, `mdpEleve`, `idClasseEleve`) VALUES
-(1, 'nomeleve1', 'prenomeleve1', 'eleve1', 'Test', 0);
+(1, 'Chevalier', 'Théo', 'Tchevalier', 'chevalit', 3),
+(2, 'Chevalier', 'Arthur', 'Achevalier', 'chevalit', 4);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_enseignant`
+-- Structure de la table `tb_enseignant`
 --
 
 DROP TABLE IF EXISTS `tb_enseignant`;
@@ -110,19 +132,20 @@ CREATE TABLE IF NOT EXISTS `tb_enseignant` (
   `Nom` varchar(50) NOT NULL,
   `Prenom` varchar(50) NOT NULL,
   PRIMARY KEY (`idUser`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `tb_enseignant`
+-- Déchargement des données de la table `tb_enseignant`
 --
 
 INSERT INTO `tb_enseignant` (`idUser`, `Identifiant`, `Motdepasse`, `Nom`, `Prenom`) VALUES
-(1, 'enseignant1', 'Test', 'nomEnseignant1', 'PrenomEnseignant1');
+(1, 'Dallier', 'Sdallier', 'Dallier', 'Sylvia'),
+(2, 'Gaullier', 'Sgaullier', 'Gaullier', 'Sébastien');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_enseigner`
+-- Structure de la table `tb_enseigner`
 --
 
 DROP TABLE IF EXISTS `tb_enseigner`;
@@ -135,10 +158,20 @@ CREATE TABLE IF NOT EXISTS `tb_enseigner` (
   KEY `idClasse` (`idClasse`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Déchargement des données de la table `tb_enseigner`
+--
+
+INSERT INTO `tb_enseigner` (`idUser`, `idMat`, `idClasse`) VALUES
+(1, 3, 3),
+(1, 3, 4),
+(2, 1, 3),
+(2, 1, 4);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_matiere`
+-- Structure de la table `tb_matiere`
 --
 
 DROP TABLE IF EXISTS `tb_matiere`;
@@ -146,12 +179,21 @@ CREATE TABLE IF NOT EXISTS `tb_matiere` (
   `idMat` int NOT NULL AUTO_INCREMENT,
   `LibelleMat` varchar(50) NOT NULL,
   PRIMARY KEY (`idMat`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `tb_matiere`
+--
+
+INSERT INTO `tb_matiere` (`idMat`, `LibelleMat`) VALUES
+(1, 'Math'),
+(2, 'Français'),
+(3, 'histoire');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_participer`
+-- Structure de la table `tb_participer`
 --
 
 DROP TABLE IF EXISTS `tb_participer`;
@@ -164,6 +206,16 @@ CREATE TABLE IF NOT EXISTS `tb_participer` (
   KEY `idControle` (`idControle`),
   KEY `idMat` (`idMat`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `tb_participer`
+--
+
+INSERT INTO `tb_participer` (`idEleve`, `idControle`, `idMat`, `Note`) VALUES
+(1, 1, 1, '18'),
+(2, 1, 1, '17'),
+(1, 2, 3, '12'),
+(2, 2, 3, '14');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
